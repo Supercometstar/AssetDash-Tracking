ac0e9694e031b429bbd3ff48263c3cf1274eb16c
Supercometstar <cometstar940409@gmail.com>
2024-12-27 21:56:27 -0800

    updapte - v1.1: duplicate fix

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -15,37 +15,52 @@ const getTransaction = async () => {
 	} catch (e) {
 		fs.writeFileSync(path.join(__dirname, '../logs/final_check.log'), '', encoding='utf-8')
 	}
-	log(`Start scrapping!`)
 	if (finalCheck) {
-		const response = await axios.get(`https://swap-api.assetdash.com/api/api_v5/whalewatch/transactions/list?page=1&limit=100&after_timestamp=${encodeURIComponent(finalCheck)}`, { headers: getHeader() })
-		response.data.transactions.pop()
-		await addTransaction(response.data.transactions)
-		if (response.data.transactions.length !== 0) {
+		let response 
+		for (let i=0; i<Number(process.env.RETRIES); i++) {
+			try {
+				response = await axios.get(`https://swap-api.assetdash.com/api/api_v5/whalewatch/transactions/list?page=1&limit=100&after_timestamp=${encodeURIComponent(finalCheck)}`, { headers: getHeader(), timeout: 5000 })
+				break
+			} catch (e) {
+				log('Retry send request')
+			}
+		}
+		if (response) {
+			response.data.transactions = response.data.transactions.filter((item) => {
+				return item.timestamp !== finalCheck
+			})
+			await addTransaction(response.data.transactions)
+			if (response.data.transactions.length !== 0) {
+				finalCheck = response.data.transactions[0].timestamp
+				fs.writeFileSync(path.join(__dirname, '../logs/final_check.log'), response.data.transactions[0].timestamp)
+			}
+		}
+	}else {
+		let response
+		for (let i=0; i<Number(process.env.RETRIES); i++) {
+			try {
+				response = await axios.get(`https://swap-api.assetdash.com/api/api_v5/whalewatch/transactions/list?page=1&limit=100`, { headers: getHeader(), timeout: 5000 })
+				break
+			} catch (e) {
+				log('Retry send request')
+			}
+		}
+		if (response) {
+			await addTransaction(response.data.transactions)
 			finalCheck = response.data.transactions[0].timestamp
 			fs.writeFileSync(path.join(__dirname, '../logs/final_check.log'), response.data.transactions[0].timestamp)
 		}
-	}else {
-		console.log()
-		const response = await axios.get(`https://swap-api.assetdash.com/api/api_v5/whalewatch/transactions/list?page=1&limit=100`, { headers: getHeader() })
-		await addTransaction(response.data.transactions)
-		finalCheck = response.data.transactions[0].timestamp
-		fs.writeFileSync(path.join(__dirname, '../logs/final_check.log'), response.data.transactions[0].timestamp)
 	}
 	setTimeout(getTransaction, 1000)
 
 }
 
 const addTransaction = async (data) => {
-	log(`${data.length} data added to MongoDB.`)
-	// data = data.map((item) => {
-	// 	return {
-	// 		timestamp: item.timestamp,
-	// 		whale: item.swap_whalewatch_list.id,
-	// 		token: item.swap_token_id,
-	// 		marketCap: item.token_market_cap,
-	// 	}
-	// })
-	console.log(data[0])
+	if (data.length === 0) {
+		log(`No data updated`)
+	}else {
+		log(`${data.length} data added to MongoDB.`)
+	}
 	try {
 		await Transaction.insertMany(data)
 	} catch (e) {
